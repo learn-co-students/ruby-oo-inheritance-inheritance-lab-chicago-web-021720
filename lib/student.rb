2e71de908a11e751deba7b32d7dbdaa38882c6d8
ellisandrews <ellisandrews1@gmail.com>
2020-02-24 11:34:20 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative 'user'
+
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(fact)
+        knowledge << fact
+    end
 
 end
